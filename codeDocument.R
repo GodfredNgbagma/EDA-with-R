79e783bec7a9d33ae9e0b84ab201982472c6e969
Godfred Ngbagma <godfredngbagma2131@gmail.com>
2024-11-12 10:27:08 +0000

    Answers updated

--- codeDocument.R
+++ codeDocument.R
@@ -3,8 +3,10 @@
 library(tidyverse)
 library(readr)
 library(dplyr)
+library(data.table)
 
-# absentees data set
+
+# loading data set
 data_set <- X10_2021PHC_CSV
 view(data_set) 
 
@@ -24,4 +26,10 @@ sub_set <- data_set %>%
   summarise(number = n(), .groups = 'drop') %>% 
   pivot_wider(names_from = a12d, values_from = number, values_fill = 0) 
 view(sub_set)
+
+#Number of male and female by  employment
+data_set1 <- fread("defactopopn_10%_20221011d.csv", select = c("p17","a11d"), na.strings = c("","NA"))
+data_set1 <- data_set1 %>% 
+  rename("Sector of employment" = p17,"Gender" = a11d)
+table(data_set1$`Sector of employment`,data_set1$Gender)
 
